3543be3c4c80da7d39e6a26582d39c51945a69aa
unknown <ni.pan1010@gmail.com>
2022-02-25 17:23:13 -0300

    Retomar da secao 1.5 da apostila 02, falando sobre vetores

---
+++ aula.js
@@ -0,0 +1,47 @@
+const a = []
+const b = a
+console.log (a === b)
+
+console.log ([] === []) // o local que cada vetor fica é diferente, o local na memoria
+console.log ([] == [])
+
+console.log (1 == [1])
+
+console.log ([] == false)
+
+console.log (null == undefined)
+
+console.log (null == null)
+
+console.log (1 == [1])
+
+console.log (true == 1)
+
+console.log (1 == 1)
+console.log (1 == "1")
+console.log (1 === 1)
+console.log (1 === "1") //não faz coerção
+
+
+const n1 = 2
+const n2 = '3'
+const n3 = n1 + n2
+//console.log(n3)
+
+const n4 = n1 + Number(n2)
+console.log(n4)
+//nome = ana
+
+//const n
+const endereco = 'rua k, 10'
+const sobrenome = `Silva`
+
+
+let a = 2
+let b = "abc"
+a = true
+a = "abc"
+
+
+var c = 2+3
+var d = "abcd"
